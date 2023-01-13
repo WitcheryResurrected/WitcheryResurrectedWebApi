a9cf5b1a4f746e9b09ed146675a7641271181042
MsRandom <ashley@msrandom.net>
2023-01-13 04:51:17 +0300

    Save suggestion content addition changes

--- WitcheryResurrectedWebApi/Controllers/SuggestionsController.cs
+++ WitcheryResurrectedWebApi/Controllers/SuggestionsController.cs
@@ -43,6 +43,8 @@ public class SuggestionsController : Controller
             CreatorName = add.CreatorName
         });
 
+        await context.SaveChangesAsync();
+
         return suggestion.Id;
     }
 
